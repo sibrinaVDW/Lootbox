6da87fb3c81f275510462c8deccbd837bc08090f
JadeyOlivier <55444202+JadeyOlivier@users.noreply.github.com>
2021-06-02 21:50:55 +0200

    Category recycler view updated to add items to list. Errors with the row.xml still need to be patched

--- app/src/main/java/com/example/lootbox/CollectionsView.kt
+++ app/src/main/java/com/example/lootbox/CollectionsView.kt
@@ -2,10 +2,34 @@ package com.example.lootbox
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class CollectionsView : AppCompatActivity() {
+
+    private var titlesList = mutableListOf<String>()
+    private var descList = mutableListOf< String>()
+    private var imagesList = mutableListOf<Int>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_collections_view)
+        postToList()
+
+        var recView :RecyclerView = findViewById(R.id.rcvCategoryList)
+        recView.layoutManager = LinearLayoutManager(this)
+        recView.adapter = Collection_RecAdapter(titlesList,descList,imagesList)
+    }
+
+    private fun addToList(title: String, desc: String , image: Int){
+        titlesList.add(title)
+        descList.add(desc)
+        imagesList.add(image)
+    }
+
+    private fun postToList(){
+        for (i in 1..25){
+            addToList("Title$i", "Description $i", R.mipmap.ic_launcher)
+        }
     }
 }
