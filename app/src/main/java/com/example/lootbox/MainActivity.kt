1ebe12f89c65b4e8f2b33db5fc439f0a79ad3eea
Sibrina <17603312@vegaconnect.co.za>
2021-06-05 19:23:20 +0200

    Separate activity created for choosing a picture from gallery and camera.

--- app/src/main/java/com/example/lootbox/MainActivity.kt
+++ app/src/main/java/com/example/lootbox/MainActivity.kt
@@ -1,48 +1,52 @@
 package com.example.lootbox
 
+import android.Manifest
 import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.firestore.FirebaseFirestore
+import java.io.File
 
 private const val REQUEST_CODE = 42
+//private lateinit var filePhoto: File
+//private const val FILE_NAME = "photo.jpg"
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val db = FirebaseFirestore.getInstance()
-        val takeAPicture  = findViewById<Button>(R.id.btnTakePicture)
+        val takeAPicture = findViewById<Button>(R.id.btnTakePicture)
         //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ID
 
-            takeAPicture.setOnClickListener {
-                val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        takeAPicture.setOnClickListener {
+            val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
 
-                if (takePictureIntent.resolveActivity(this.packageManager) != null) {
-                    startActivityForResult(takePictureIntent, REQUEST_CODE)
-                } else {
-                    Toast.makeText(this, "Unable to open camera", Toast.LENGTH_LONG).show()
-                }
+            if (takePictureIntent.resolveActivity(this.packageManager) != null) {
+                startActivityForResult(takePictureIntent, REQUEST_CODE)
+            } else {
+                Toast.makeText(this, "Unable to open camera", Toast.LENGTH_LONG).show()
             }
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        val viewImage  = findViewById<ImageView>(R.id.imageView01)
-        //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ID
-        if (requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK)
-        {
-           val takenImage = data?.extras?.get("data") as Bitmap
-            viewImage.setImageBitmap(takenImage)
-        } else{
-            super.onActivityResult(requestCode, resultCode, data)
         }
-
     }
-
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val viewImage = findViewById<ImageView>(R.id.imageView01)
+        //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ID
+        if (requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            val takenImage = data?.extras?.get("data") as Bitmap
+            viewImage.setImageBitmap(takenImage)
+        } else {
+            super.onActivityResult(requestCode, requestCode, data)
+        }
+    }
 }
