fe4541d7c26b644ba86959234e2737ce2f1162c6
Sibrina <17603312@vegaconnect.co.za>
2021-06-05 20:36:19 +0200

    Update

--- app/src/main/java/com/example/lootbox/MainActivity.kt
+++ app/src/main/java/com/example/lootbox/MainActivity.kt
@@ -1,52 +1,173 @@
 package com.example.lootbox
 
-import android.Manifest
+import android.annotation.TargetApi
 import android.app.Activity
+import android.content.ContentUris
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
+
 import android.os.Build
 import android.os.Bundle
-import android.os.Environment
+import android.provider.DocumentsContract
 import android.provider.MediaStore
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.firestore.FirebaseFirestore
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import java.io.File
 
-private const val REQUEST_CODE = 42
-//private lateinit var filePhoto: File
-//private const val FILE_NAME = "photo.jpg"
 
 class MainActivity : AppCompatActivity() {
+    //Our variables
+    private var mImageView: ImageView? = null
+    private var mUri: Uri? = null
+    //Our widgets
+    private lateinit var btnCapture: Button
+    private lateinit var btnChoose : Button
+    //Our constants
+    private val OPERATION_CAPTURE_PHOTO = 1
+    private val OPERATION_CHOOSE_PHOTO = 2
+
+
+    private fun initializeWidgets() {
+        btnCapture = findViewById(R.id.btnCapture)
+        btnChoose = findViewById(R.id.btnChoose)
+        mImageView = findViewById(R.id.mImageView)
+    }
+
+    private fun show(message: String) {
+        Toast.makeText(this,message,Toast.LENGTH_SHORT).show()
+    }
+    private fun capturePhoto(){
+        val capturedImage = File(externalCacheDir, "My_Captured_Photo.jpg")
+        if(capturedImage.exists()) {
+            capturedImage.delete()
+        }
+        capturedImage.createNewFile()
+        mUri = if(Build.VERSION.SDK_INT >= 24){
+            FileProvider.getUriForFile(this, "info.camposha.kimagepicker.fileprovider",
+                capturedImage)
+        } else {
+            Uri.fromFile(capturedImage)
+        }
+
+        val intent = Intent("android.media.action.IMAGE_CAPTURE")
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, mUri)
+        startActivityForResult(intent, OPERATION_CAPTURE_PHOTO)
+    }
+    private fun openGallery(){
+        val intent = Intent("android.intent.action.GET_CONTENT")
+        intent.type = "image/*"
+        startActivityForResult(intent, OPERATION_CHOOSE_PHOTO)
+    }
+    private fun renderImage(imagePath: String?){
+        if (imagePath != null) {
+            val bitmap = BitmapFactory.decodeFile(imagePath)
+            mImageView?.setImageBitmap(bitmap)
+        }
+        else {
+            show("ImagePath is null")
+        }
+    }
+    private fun getImagePath(uri: Uri?, selection: String?): String {
+        var path: String? = null
+        val cursor = uri?.let { contentResolver.query(it, null, selection, null, null ) }
+        if (cursor != null){
+            if (cursor.moveToFirst()) {
+                path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA))
+            }
+            cursor.close()
+        }
+        return path!!
+    }
+    @TargetApi(19)
+    private fun handleImageOnKitkat(data: Intent?) {
+        var imagePath: String? = null
+        val uri = data!!.data
+        //DocumentsContract defines the contract between a documents provider and the platform.
+        if (DocumentsContract.isDocumentUri(this, uri)){
+            val docId = DocumentsContract.getDocumentId(uri)
+            if (uri != null) {
+                if ("com.android.providers.media.documents" == uri.authority){
+                    val id = docId.split(":")[1]
+                    val selsetion = MediaStore.Images.Media._ID + "=" + id
+                    imagePath = getImagePath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                        selsetion)
+                } else if ("com.android.providers.downloads.documents" == uri.authority){
+                    val contentUri = ContentUris.withAppendedId(Uri.parse(
+                        "content://downloads/public_downloads"), java.lang.Long.valueOf(docId))
+                    imagePath = getImagePath(contentUri, null)
+                }
+            }
+        }
+        else if (uri != null) {
+            if ("content".equals(uri.scheme, ignoreCase = true)){
+                imagePath = getImagePath(uri, null)
+            }
+            else if ("file".equals(uri.scheme, ignoreCase = true)){
+                imagePath = uri.path
+            }
+        }
+        renderImage(imagePath)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>
+                                            , grantedResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantedResults)
+        when(requestCode){
+            1 ->
+                if (grantedResults.isNotEmpty() && grantedResults.get(0) ==
+                    PackageManager.PERMISSION_GRANTED){
+                    openGallery()
+                }else {
+                    show("Unfortunately You are Denied Permission to Perform this Operataion.")
+                }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when(requestCode){
+            OPERATION_CAPTURE_PHOTO ->
+                if (resultCode == Activity.RESULT_OK) {
+                    val bitmap = BitmapFactory.decodeStream(
+                        mUri?.let { getContentResolver().openInputStream(it) })
+                    mImageView!!.setImageBitmap(bitmap)
+                }
+            OPERATION_CHOOSE_PHOTO ->
+                if (resultCode == Activity.RESULT_OK) {
+                    if (Build.VERSION.SDK_INT >= 19) {
+                        handleImageOnKitkat(data)
+                    }
+                }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val db = FirebaseFirestore.getInstance()
-        val takeAPicture = findViewById<Button>(R.id.btnTakePicture)
-        //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ID
+        initializeWidgets()
 
-        takeAPicture.setOnClickListener {
-            val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-
-            if (takePictureIntent.resolveActivity(this.packageManager) != null) {
-                startActivityForResult(takePictureIntent, REQUEST_CODE)
-            } else {
-                Toast.makeText(this, "Unable to open camera", Toast.LENGTH_LONG).show()
+        btnCapture.setOnClickListener{capturePhoto()}
+        btnChoose.setOnClickListener{
+            //check permission at runtime
+            val checkSelfPermission = ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            if (checkSelfPermission != PackageManager.PERMISSION_GRANTED){
+                //Requests permissions to be granted to this application at runtime
+                ActivityCompat.requestPermissions(this,
+                    arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE), 1)
+            }
+            else{
+                openGallery()
             }
         }
     }
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        val viewImage = findViewById<ImageView>(R.id.imageView01)
-        //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ID
-        if (requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK) {
-            val takenImage = data?.extras?.get("data") as Bitmap
-            viewImage.setImageBitmap(takenImage)
-        } else {
-            super.onActivityResult(requestCode, requestCode, data)
-        }
-    }
 }
+//end
