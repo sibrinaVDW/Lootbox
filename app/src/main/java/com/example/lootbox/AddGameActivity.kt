450be71bc3fcddaf78c806e5aeba535b8dae2a27
joyst <ernest178.em@gmail.com>
2021-06-06 10:32:03 +0200

    Ernest_AddGameActivity

---
+++ app/src/main/java/com/example/lootbox/AddGameActivity.kt
@@ -0,0 +1,38 @@
+package com.example.lootbox
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class AddGameActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_game)
+
+        val itemListActivity = ItemListActivity()
+    //    itemListActivity.postToItemList("Title","Description",R.mipmap.ic_launcher_round)
+
+        val btnAddToGame : Button = findViewById(R.id.btnAddGame)
+        btnAddToGame.setOnClickListener {
+
+
+            val gameName : TextView = findViewById(R.id.txtGameName)
+            val gameDescription : TextView = findViewById(R.id.txtGameDescription)
+            val gameImage : Int = R.mipmap.ic_launcher_round
+
+            val intent = Intent(this, ItemListActivity::class.java).apply {
+
+            }
+           itemListActivity.postToItemList(gameName.text.toString(),gameDescription.text.toString(), R.mipmap.ic_launcher_round)
+       //     itemListActivity.postToItemList("Name","Description",R.mipmap.ic_launcher_round)
+            startActivity(intent)
+
+        }
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/lootbox/ItemsRecyclerAdapter.kt
@@ -0,0 +1,52 @@
+package com.example.lootbox
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+
+class ItemsRecyclerAdapter(
+    private var gameTitles: List<String>,
+    private var gameDescriptions: List<String>,
+    private var gameImages: List<Int>
+) :
+    RecyclerView.Adapter<ItemsRecyclerAdapter.ViewHolder>() {
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val itemTitle: TextView = itemView.findViewById(R.id.txtTitle)
+        val itemDescription: TextView = itemView.findViewById(R.id.txtDescription)
+        val itemPicture: ImageView = itemView.findViewById(R.id.imgThumbNail)
+
+        //this is where I am
+        init {
+            itemView.setOnClickListener { v: View ->
+                val position: Int = adapterPosition
+                Toast.makeText(
+                    itemView.context,
+                    "You clicked on item # ${position + 1}",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val v: View =
+            LayoutInflater.from(parent.context).inflate(R.layout.activity_item_list_item, parent, false)
+        return ViewHolder(v)
+    }
+
+    override fun getItemCount(): Int {
+        return gameTitles.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.itemTitle.text = gameTitles[position]
+        holder.itemDescription.text = gameDescriptions[position]
+        holder.itemPicture.setImageResource(gameImages[position])
+    }
+
+}
